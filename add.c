d623ea85b80359e96a7e9f74139d832dc8dade77
LAPTOP-CSLTPSM8\Emil <guldskytt04@hotmail.com>
2024-09-17 17:03:10 +0200

    fixing_add

--- add.c
+++ add.c
@@ -1,9 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int is_number(const char *str) { 
+    char *endptr; 
+    double value = strtod(str, &endptr); 
+ 
+    // Check if the entire string was a valid number 
+    if (endptr == str) { 
+        return 0; // Not a number 
+    } 
+ 
+    return 1; // Is a number 
+} 
+
+
 int main(int argc, char *argv[]){
+  
   int a = atoi(argv[1]);
   int b = atoi(argv[2]);
-  printf("%d\n", a+b);
+  if(is_number(argv[1]))
+  {
+    if(is_number(argv[2])){
+      printf("%d\n", a+b);
+    }
+    else{
+      printf("-ERROR-\n");
+    }
+  }
+  else{
+    printf("-ERROR-\n");
+  }
+  
   return 0;
 }
